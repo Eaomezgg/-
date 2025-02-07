9fe6c8b1a8065e88941737236edc6d9230991fb1
Eaomezgg <65010047@kmitl.ac.th>
2025-02-07 18:13:19 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function opencart()
+{
+    $('#modalcart').css('display','flex')
+}
+
+function openproductdetail()
+{
+    $('#modaldesc').css('display','flex')
+}
+
+function closemodal(){
+    $(".modal").css('display','none')
+}
